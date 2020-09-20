9f67dce9abd7763c9dd6c0575aae68c88a9afccd
SatYu26 <goyalsatyam8@gmail.com>
2020-09-20 20:28:23 +0530

    ADB solver

--- solver.py
+++ solver.py
@@ -1,16 +1,3 @@
-board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
-]
-
-# final function for solving the sudoku
 def solve(bo):
     find = find_empty(bo)
     if not find:
@@ -29,7 +16,7 @@ def solve(bo):
 
     return False
 
-# Check the validity of the board
+
 def valid(bo, num, pos):
     # Check row
     for i in range(len(bo[0])):
@@ -52,7 +39,7 @@ def valid(bo, num, pos):
 
     return True
 
-# for printing the board systematically
+
 def print_board(bo):
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
@@ -67,7 +54,7 @@ def print_board(bo):
             else:
                 print(str(bo[i][j]) + " ", end="")
 
-# for finding empty posotions in the board
+
 def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
@@ -75,8 +62,3 @@ def find_empty(bo):
                 return (i, j)  # row, col
 
     return None
-
-print_board(board)
-solve(board)
-print("___________________")
-print_board(board)
